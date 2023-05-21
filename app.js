426013c244300639305d73e06f4291e38f037569
Amauri22 <mar.hamaekers@gmail.com>
2023-05-21 10:16:32 +0200

    form finished

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ const closeMenu = document.getElementById("x");
 const tabTitles = document.querySelector(".tab-titles");
 const tabLinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyRNEONVxCf4qJRsXzb9_IPUGT6OLWrFrVE6bCJnRhyJ0LssuVmLMKpDaNgXECWunwO/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response =>{
+            msg.innerHTML = "Message sent successfully";
+            setTimeout(() => msg.innerHTML = "",5000);
+            form.reset();
 
+        })
+        .catch(error => console.error('Error!', error.message))
+})
 
 
 openMenu.addEventListener("click", openSidebar);
 
-closeMenu.addEventListener("click", closeSidebar)
+closeMenu.addEventListener("click", closeSidebar);
+
 
 function openSidebar(){
     navBar.style.right = "0";
@@ -20,6 +34,8 @@ function openSidebar(){
 function closeSidebar(){
     navBar.style.right = "-200px";
 }
+
+
 // tabTitles.addEventListener("click", function(e) {
 //     console.log(e.target, "i got clicked")
 //     console.log(e.currentTarget);
